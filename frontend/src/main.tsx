6c3a8a03e537197ec11d10f3fb55e52116b30c73
desonglll <lindesong666@163.com>
2024-02-24 23:15:06 +0800

    Change baseURL in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,10 +4,10 @@ import App from "./App.tsx";
 import "./index.css";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://192.168.31.207:8000";
+axios.defaults.baseURL = "http://localhost:8000";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
